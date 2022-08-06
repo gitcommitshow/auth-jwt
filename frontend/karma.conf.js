e00e8cd0effe0428c3b0b29601d48560fa68aa4d
gitcommitshow <pradeep@gitcommit.show>
2022-08-06 20:52:15 +0530

    fix: content links and styling issues

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/supertokens'),
+      dir: require('path').join(__dirname, './coverage/frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
